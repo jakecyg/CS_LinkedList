c9727da1fe36342c3183156905a87607bb287d8f
Jake Choi <JakeCYG@gmail.com>
2020-12-07 22:51:30 -0600

    added employee class

---
+++ LinkedList/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Employee : IComparable<Employee>
+    {
+        internal int EmployeeID { get; }
+        internal string FirstName { get; }
+        internal string LastName { get; }
+
+        public Employee(int employeeId, string firstName = null, string lastName = null)
+        {
+            this.EmployeeID = employeeId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public int CompareTo(Employee other) => EmployeeID.CompareTo(other.EmployeeID);
+
+        public override string ToString() => $"{EmployeeID} {(FirstName ?? "null")} {(LastName ?? "null")}";
+
+    }
+}
